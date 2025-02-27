f7d46f3a471a96de2ecef11cfb01835b7ea1708c
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-02-27 15:59:02 +0600

    updated

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export type UserName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  mtherContactNo: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactName: string;
+  address: string;
+};
+
+export type IStudent = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  email: string;
+  dateOfBirth?: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'O+' | 'O-' | 'AB+' | 'AB-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
